4197980ac4f93510e4e33339a8e37534b2a15991
470281149@qq.com <470281149@qq.com>
2019-06-20 11:42:28 +0800

    初次提交

---
+++ src/common/js/config.js
@@ -0,0 +1,8 @@
+export const HOST = 'https://net-music.penkuoer.com/'
+export const ERR_OK = 200
+
+export const playMode = {
+  sequence: 0,
+  loop: 1,
+  random: 2
+}
---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="m-header">
+    <img src="../../common/image/logo.png" alt="">
+    <h1 class="text">&nbsp;Tx Music</h1>
+    <router-link to="/user" class="mine" tag="div">
+     <i class="iconfont icon-list"></i>
+    </router-link>
+    <router-link to="/search" class="search" tag="div">
+      <i class="iconfont icon-search"></i>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~common/scss/variable.scss";
+
+.m-header {
+  position: relative;
+  height: 44px;
+  width: 100%;
+  text-align: center;
+  background: $color-theme;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  img{
+    width: 4vh;
+  }
+  .text {
+    line-height: 44px;
+    font-weight: bold;
+    color: $color-text-l;
+    letter-spacing: 3px;
+  }
+  .mine {
+    position: absolute;
+    top: -1.5px;
+    left: 0;
+    .iconfont {
+        display: block;
+        padding: 11px;
+        font-size: 22px;
+        color: $color-theme-l;
+    }
+  }
+  .search {
+    position: absolute;
+    top: 0;
+    right: 2px;
+    .iconfont {
+        display: block;
+        padding: 12px;
+        font-size: 18px;
+        color: $color-theme-l;
+    }
+  }
+}
+</style>
